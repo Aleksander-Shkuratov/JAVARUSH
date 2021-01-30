3767243ee6fdd6a79f72b6d8af4e85ad75271d12
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-01-30 22:20:56 +0300

    Create task0111.java

---
+++ JavaSyntax/level1/task0111.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task01.task0111;
+
+/* 
+Одной переменной недостаточно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a=20;
+        int b=30;
+        String s="Son";
+        //напишите тут ваш код
+    }
+}
