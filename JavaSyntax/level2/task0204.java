b421dd818fb062d6706eea6110fca3889a4eefa0
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-13 22:53:15 +0300

    Create task0204.java

---
+++ JavaSyntax/level2/task0204.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task02.task0204;
+
+/* 
+О семейных отношениях
+
+В методе main создай объект Man,
+сохрани ссылку на него в переменную man.
+Создай также объект Woman
+и сохрани ссылку на него в переменную woman.
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Man man = new Man();
+        Woman woman = new Woman();
+        man.wife = woman;
+        woman.husband = man;
+        //напишите тут ваш код
+    }
+
+    public static class Man {
+        public int age;
+        public int height;
+        public Woman wife;
+    }
+
+    public static class Woman {
+        public int age;
+        public int height;
+        public Man husband;
+    }
+}
