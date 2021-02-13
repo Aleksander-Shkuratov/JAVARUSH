6e83b6f49d1abcaf263db814cee9f10e554f5ee9
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-13 08:23:50 +0300

    Create task0127.java

---
+++ JavaSyntax/level1/task0127.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task01.task0127;
+
+/* 
+Число в квадрате
+
+Напиши программу, которая выводит на экран 
+квадрат числа 5 с помощью метода sqr.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(sqr(5));
+        //напишите тут ваш код
+    }
+
+    public static int sqr(int a) {
+        return a * a;
+    }
+}
