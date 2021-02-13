3a246b5bf34064164e1ec9fed9268430e3a1870a
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-13 08:26:08 +0300

    Create task0135.java

---
+++ JavaSyntax/level1/task0135.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task01.task0135;
+
+/* 
+Выбирай здоровую еду! Выбирай фрукты!
+
+Выведи на экран переменные, 
+значения которых являются названиями фруктов.
+Каждую переменную выведи с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String apple = "Яблоко";
+        String porridge = "Каша";
+        String sausage = "Колбаса";
+        String peach = "Персик";
+        System.out.println(apple);
+        System.out.println(peach);
+
+        //напишите тут ваш код
+    }
+}
