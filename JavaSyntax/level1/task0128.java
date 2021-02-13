26c4aecb9bbb0822fee4681cb8096031ef01acb4
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-13 08:25:03 +0300

    Create task0128.java

---
+++ JavaSyntax/level1/task0128.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task01.task0128;
+
+/* 
+Просто, как 2+2
+
+Сложи 2 и 2 с помощью метода sum.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        sum(2,2);
+        //напишите тут ваш код
+    }
+
+    public static void sum(int a, int b) {
+        int c = a + b;
+        System.out.print(c);
+    }
+}
