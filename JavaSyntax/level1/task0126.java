9067d9829947d37fe9b100e6c2cc01658583080c
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-13 08:22:30 +0300

    Create task0126.java

---
+++ JavaSyntax/level1/task0126.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task01.task0126;
+
+/* 
+Не хочешь  - решай нехотя
+
+Напиши программу, которая выводит на экран надпись:
+"Хочешь - решай задачи, не хочешь - решай нехотя" 16 раз.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String s="Хочешь - решай задачи, не хочешь - решай нехотя";
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        //напишите тут ваш код
+    }
+}
