f656618dfc884dad4786402115633eb8df37278a
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-13 22:57:56 +0300

    Create task0207.java

---
+++ JavaSyntax/level2/task0207.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task02.task0207;
+
+/* 
+Кусочек калькулятора
+
+В методе main объяви две переменные a и b типа int.
+Присвой им любые значения.
+Выведи на экран их сумму и произведение.
+Каждое значение выводить с новой строки.
+*/
+public class Solution {
+    public static void main(String[] args) {
+        int a=5;
+        int b=7;
+        System.out.println(a+b);
+        System.out.println(a*b);
+        //напишите тут ваш код
+    }
+}
