eb6d8b33691b5418cb50f692bd4a854ba54fca7c
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-14 21:14:44 +0300

    Create task0210.java

---
+++ JavaSyntax/level2/task0210.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task02.task0210;
+
+/* 
+Необъективная реальность
+
+Напиши программу, которая выводит на экран надпись:
+"Будешь плохо кодить, придет Java и съест твою память".
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Будешь плохо кодить, придет Java и съест твою память");
+        //напишите тут ваш код
+    }
+}
