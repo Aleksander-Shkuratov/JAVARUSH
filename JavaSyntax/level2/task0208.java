7214da2dc299fadeecdb16b88ae4998ee77f6c91
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-14 21:11:43 +0300

    Create task0208.java

---
+++ JavaSyntax/level2/task0208.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task02.task0208;
+
+/* 
+Одного кота нам мало
+
+Создать объект типа Cat 2 раза.
+Сохрани каждый экземпляр в свою переменную.
+Имена переменных должны быть разные.
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Cat murzik = new Cat();
+        Cat vaska = new Cat();
+        //напишите тут ваш код
+    }
+
+    public static class Cat {
+
+    }
+}
