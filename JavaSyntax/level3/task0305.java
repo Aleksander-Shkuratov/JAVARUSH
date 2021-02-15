6755481d350445ff31932cefe84989e43d1f5266
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-15 22:58:42 +0300

    Birthday

---
+++ JavaSyntax/level3/task0305.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task03.task0305;
+
+/* 
+Я так давно родился
+
+Вывести на экран дату своего рождения в виде:
+MAY 1 2012
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("DECEMBER "+"25 "+"1981");
+        //напишите тут ваш код
+
+    }
+}
