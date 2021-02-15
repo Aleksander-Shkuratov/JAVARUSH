343b80fc8dd2d4aae6dc2289787db6677d97a62b
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-15 23:08:30 +0300

    sum of 5 numbers

---
+++ JavaSyntax/level3/task0309.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task03.task0309;
+
+/* 
+Сумма 5 чисел
+
+Вывести на экран сумму чисел от 1 до 5 построчно (должно быть 5 строк):
+1
+1+2=3
+1+2+3=6
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(1+2);
+        System.out.println(1+2+3);
+        System.out.println(1+2+3+4);
+        System.out.println(1+2+3+4+5);
+        //напишите тут ваш код
+
+    }
+}
