63417a8562310d3874eba741accb4e839bd26b87
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-15 11:10:43 +0300

    creating a print3 method

---
+++ JavaSyntax/level2/task0218.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task02.task0218;
+
+/* 
+Повторенье-мать
+
+Реализуй метод print3. 
+Метод должен вывести на экран переданную строку 3 раза.
+Каждый раз с новой строки.
+*/
+public class Solution {
+    public static void print3(String s) {
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        //напишите тут ваш код
+
+    }
+
+    public static void main(String[] args) {
+        print3("I love you!");
+    }
+}
