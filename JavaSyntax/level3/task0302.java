aeb270bf7742f47d19d0e0fcb2c19bd6220c3931
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-15 11:48:14 +0300

    Create method printString

---
+++ JavaSyntax/level3/task0302.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task03.task0302;
+
+/* 
+Немедленно в печать
+
+Добавь метод public static void printString(String s),
+в нем напиши код, который будет выводить переданную строку на экран.
+*/
+
+public class Solution {
+    
+    public static void printString(String a){
+        System.out.println(a);
+    }
+    // напишите тут ваш код
+
+    public static void main(String[] args) {
+        printString("Hello, Amigo!");
+    }
+}
