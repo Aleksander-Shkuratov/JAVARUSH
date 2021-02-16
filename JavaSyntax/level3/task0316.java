230f173ec6fd6ef30cf94ade435a9498b60b5fbc
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-16 23:13:49 +0300

    Escape characters

---
+++ JavaSyntax/level3/task0316.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task03.task0316;
+
+/* 
+Экранирование символов
+
+Вывести на экран следующий текст - две строки:
+It's a Windows path: "C:\Program Files\Java\jdk1.7.0\bin"
+It's a Java string: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("It\'s Windows path: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"");
+        System.out.println("It\'s Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\\\"");
+        //напишите тут ваш код
+
+    }
+}
