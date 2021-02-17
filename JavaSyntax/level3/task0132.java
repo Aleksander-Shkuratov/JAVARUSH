7f6e9d20a983b220e739a5d324f14535a4c07859
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-17 13:05:48 +0300

    The sum of the digits of a three-digit number

---
+++ JavaSyntax/level3/task0132.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task01.task0132;
+
+/* 
+Сумма цифр трехзначного числа
+
+Напиши код метода sumDigitsInNumber(int number).
+Метод на вход принимает целое трехзначное число.
+Нужно посчитать сумму цифр этого числа, и вернуть эту сумму.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(sumDigitsInNumber(546));
+    }
+
+    public static int sumDigitsInNumber(int number) {
+        int a = number/100;
+        int b = (number - a*100)/10;
+        int c = number - a*100 - b*10;
+        return(a+b+c);
+        //напишите тут ваш код
+    }
+}
