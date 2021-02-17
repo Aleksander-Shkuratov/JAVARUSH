7a3676535f5efc3f000371617373f9f9457f40a0
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-17 13:00:03 +0300

    full meter

---
+++ JavaSyntax/level3/task0131.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task01.task0131;
+
+/* 
+Полнометражная картина
+
+Реализуй метод getMetreFromCentimetre(int centimetre).
+Метод на вход принимает количество сантиметров.
+Нужно сделать так, 
+чтобы метод возвращал число полных метров в centimetre. (1 метр = 100 см).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getMetreFromCentimetre(243));
+    }
+
+    public static int getMetreFromCentimetre(int centimetre) {
+        return centimetre/100;
+        //напишите тут ваш код
+    }
+}
