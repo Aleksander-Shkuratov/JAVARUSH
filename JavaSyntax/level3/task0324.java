74a7ac6ab8f2cf6ac896f667f0612a9dae0ca38f
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-17 13:01:24 +0300

    Mercantile intentions

---
+++ JavaSyntax/level3/task0324.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task03.task0324;
+
+/* 
+Меркантильные намерения
+
+Вывести на экран десять раз надпись 
+"Я хочу большую зарплату, и для этого изучаю Java"
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        //напишите тут ваш код
+    }
+}
