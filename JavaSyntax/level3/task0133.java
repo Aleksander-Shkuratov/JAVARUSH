ce3f96094a2ffc7f87452a64c5e7d9e3c5b9fec3
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-17 12:57:31 +0300

    Don't think about seconds

---
+++ JavaSyntax/level3/task0133.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0133;
+
+/* 
+Не думать о секундах…
+
+Напиши код, который считает сколько секунд прошло с 15:00,
+если на часах 15:30. Выведи результат на экран.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int secondsAfter15 = 0;
+        secondsAfter15 = 30*60;
+        System.out.println(secondsAfter15);
+    }
+}
