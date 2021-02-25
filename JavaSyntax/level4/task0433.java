73b40b7858f1a0df82f6549e6b811d4019589417
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-25 07:32:49 +0300

    Square of the letters

---
+++ JavaSyntax/level4/task0433.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task04.task0433;
+
+/* 
+Гадание на долларовый счет
+
+Вывести на экран квадрат из 10х10 букв S используя цикл while.
+Буквы в каждой строке не разделять.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        int n = 10;
+        
+        while(n > 0){
+            System.out.println("SSSSSSSSSS");
+            n = n - 1;
+        }
+        //напишите тут ваш код
+    }
+}
