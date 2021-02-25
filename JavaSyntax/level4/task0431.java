99f2bf5b5988658d319ba34e5154889edaeb5b12
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-25 07:27:52 +0300

    From ten to one

---
+++ JavaSyntax/level4/task0431.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task04.task0431;
+
+/* 
+От 10 до 1
+
+Вывести на экран числа от 10 до 1 включительно,
+используя цикл while.
+Каждое значение с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        
+        while(a > 0){
+            System.out.println(a);
+            a = a - 1;
+        }
+        //напишите тут ваш код
+    }
+}
