92ca51784e780947c3c02b051f9383f86a01e192
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-02-26 12:44:35 +0300

    Declaration of love

---
+++ JavaSyntax/level4/task0439.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task04.task0439;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Письмо счастья
+
+Ввести с клавиатуры имя и используя цикл for 10 раз вывести: <имя> любит меня.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        
+        for(int i = 0; i < 10; i++){
+            System.out.println(name +" любит меня.");
+        }
+        //напишите тут ваш код
+    }
+}
