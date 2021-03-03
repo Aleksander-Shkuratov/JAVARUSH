ac63f845624512307de6c1fb0f431e53c71d57b1
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-03-03 07:12:08 +0300

    Man and Woman

---
+++ JavaSyntax/level5/task0526.java
@@ -0,0 +1,73 @@
+package com.javarush.task.task05.task0526;
+
+/* 
+Мужчина и женщина
+
+1. Внутри класса Solution создай public static классы Man и Woman.
+2. У классов должны быть поля: name (String), age (int), address (String).
+3. Создай конструкторы, в которые передаются все возможные параметры.
+4. Создай по два объекта каждого класса со всеми данными используя конструктор.
+5. Объекты выведи на экран в таком формате: name + " " + age + " " + address
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Man man1 = new Man("Alex", 20, "Moscow");
+        Man man2 = new Man("Max", 25, "Vladimir");
+        System.out.println(man1.name+" "+man1.age+" "+man1.address);
+        System.out.println(man2.name+" "+man2.age+" "+man2.address);
+
+        Woman woman1 = new Woman("Mari", 27,"Minsk");
+        Woman woman2 = new Woman("Olga",24,"Gomel");
+        System.out.println(woman1.name+" "+woman1.age+" "+woman1.address);
+        System.out.println(woman2.name+" "+woman2.age+" "+woman2.address);
+
+        //напишите тут ваш код
+    }
+
+    public static class Man{
+        String name;
+        int age;
+        String address;
+
+        public Man(){
+
+        }
+        public Man(String name){
+            this.name = name;
+        }
+        public Man(String name, int age){
+            this.name = name;
+            this.age = age;
+        }
+        public Man(String name, int age, String address){
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+    }
+
+    public static class Woman{
+        String name;
+        int age;
+        String address;
+
+        public Woman(){
+
+        }
+        public Woman(String name){
+            this.name = name;
+        }
+        public Woman(String name, int age){
+            this.name = name;
+            this.age = age;
+        }
+        public Woman(String name, int age, String address){
+            this.name = name;
+            this.age = age;
+            this.address= address;
+        }
+    }
+    //напишите тут ваш код
+}
