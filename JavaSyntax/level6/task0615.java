553671e0a9304bbfe3e5e895326584c4332af0ae
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-03-05 21:46:38 +0300

    Static

---
+++ JavaSyntax/level6/task0615.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task06.task0615;
+
+/* 
+Феншуй и статики
+
+Переставь один модификатор static,
+чтобы пример скомпилировался.
+*/
+
+public class Solution {
+
+    static public int A = 5;
+    public int B = 2;
+    public int C = A * B;
+
+    public static void main(String[] args) {
+        A = 15;
+    }
+}
