f1cdb05061c308f60e8eb1c40c7cd644121261c4
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-03-06 08:23:29 +0300

    Related relationships of cats

---
+++ JavaSyntax/level6/task0621.java
@@ -0,0 +1,82 @@
+package com.javarush.task.task06.task0621;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Родственные связи кошек
+
+Задача: У каждой кошки есть имя и кошка-мама.
+Создать класс, который бы описывал данную ситуацию.
+Создать два объекта: кошку-дочь и кошку-маму.
+Вывести их на экран.
+
+Новая задача: У каждой кошки есть имя, кот-папа и кошка-мама.
+Изменить класс Cat так, чтобы он мог описать данную ситуацию.
+Создать 6 объектов: дедушку (папин папа), бабушку (мамина мама), папу, маму, сына, дочь.
+Вывести их всех на экран в порядке: дедушка, бабушка, папа, мама, сын, дочь.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String grandfatherName = reader.readLine();
+        Cat catGrandfather = new Cat(grandfatherName, null, null);
+
+        String grandmotherName = reader.readLine();
+        Cat catGrandmother = new Cat(grandmotherName, null, null);
+
+        String fatherName = reader.readLine();
+        Cat catFather = new Cat(fatherName, catGrandfather,null);
+
+        String motherName = reader.readLine();
+        Cat catMother = new Cat(motherName, null, catGrandmother);
+
+        String sonName = reader.readLine();
+        Cat catSon = new Cat(sonName, catFather, catMother);
+
+        String daughterName = reader.readLine();
+        Cat catDaughter = new Cat(daughterName, catFather, catMother);
+
+        System.out.println(catGrandfather);
+        System.out.println(catGrandmother);
+        System.out.println(catFather);
+        System.out.println(catMother);
+        System.out.println(catSon);
+        System.out.println(catDaughter);
+    }
+
+    public static class Cat {
+        private String name;
+        private Cat mother;
+        private Cat father;
+
+        Cat(String name) {
+            this.name = name;
+        }
+
+        Cat(String name, Cat father,Cat mother) {
+            this.name = name;
+            this.father = father;
+            this.mother = mother;
+        }
+
+        @Override
+        public String toString() {
+            if (mother == null && father == null )
+                return "The cat's name is " + name + ", no mother, no father ";
+            else if (mother == null){
+                return "The cat's name is " + name + ", no mother, father is " + father.name;
+            }
+            else if (father == null){
+                return "The cat's name is " + name + " mother is " + mother.name + ", no father";
+            }
+            else{
+                return "The cat's name is " + name + ", mother is " + mother.name + ", father is " + father.name;
+            }
+        }
+    }
+
+}
