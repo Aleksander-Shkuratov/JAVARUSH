5c304a6209cc6622ca99db48cb8b0d272a265897
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-03-12 21:22:32 +0300

    LinkedList and ArrayList.

---
+++ JavaSyntax/level8/task0807.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task08.task0807;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/* 
+LinkedList и ArrayList
+
+Нужно создать два списка - LinkedList и ArrayList.
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        ArrayList<String> listA = new ArrayList<String>();
+        
+        return listA;
+        //напишите тут ваш код
+    }
+
+    public static Object createLinkedList() {
+        LinkedList<String> listL = new LinkedList<String>();
+        
+        return listL;
+        //напишите тут ваш код
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
