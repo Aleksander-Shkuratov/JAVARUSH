be61a4ffcbe22c6f40cfb0fd90bc4d8adf72caf5
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-03-13 13:36:33 +0300

    ArrayList or LinkedList

---
+++ JavaSyntax/level8/task0811.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task08.task0811;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Квартет «Методы»
+
+Реализовать 4 метода.
+Каждый из них должен возвращать список, 
+который лучше всего подходит для выполнения данных операций
+(быстрее всего справится с большим количеством операций).
+Ничего измерять не нужно.
+*/
+
+public class Solution {
+    public static List getListForGet() {
+        return new ArrayList<>();
+        //напишите тут ваш код
+    }
+
+    public static List getListForSet() {
+        return new ArrayList<>();
+        //напишите тут ваш код
+    }
+
+    public static List getListForAddOrInsert() {
+        return new LinkedList<>();
+        //напишите тут ваш код
+    }
+
+    public static List getListForRemove() {
+        return new LinkedList<>();
+        //напишите тут ваш код
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
