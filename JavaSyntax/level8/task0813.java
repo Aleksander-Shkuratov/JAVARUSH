5a02fbf6856ad8c2f8ee45c6a7110a87e9f52ec3
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-03-14 07:59:49 +0300

    20 words

---
+++ JavaSyntax/level8/task0813.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+20 слов на букву «Л»
+
+Создать множество строк (Set<String>),
+занести в него 20 слов на букву "Л".
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        Set<String> set = new HashSet<String>();
+        set.add("Лещ");
+        set.add("Лось");
+        set.add("Лев");
+        set.add("Лом");
+        set.add("Лаз");
+        set.add("Лунь");
+        set.add("Лиса");
+        set.add("Любовь");
+        set.add("Лик");
+        set.add("Лай");
+        
+        set.add("Лайка");
+        set.add("Лист");
+        set.add("Лубок");
+        set.add("Ломоть");
+        set.add("Литий");
+        set.add("Литера");
+        set.add("Лес");
+        set.add("Лесник");
+        set.add("Леший");
+        set.add("Лукошко");
+        
+        return set;
+        
+        //напишите тут ваш код
+    
+    }
