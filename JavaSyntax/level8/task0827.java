7ac2fd0e1e5958b4c39c21aac6cb23321872d51f
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-03-20 20:39:56 +0300

    Date

---
+++ JavaSyntax/level8/task0827.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task08.task0827;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+/* 
+Работа с датой
+
+1. Реализовать метод isDateOdd(String date) так, 
+чтобы он возвращал true, 
+если количество дней с начала года - нечетное число, иначе false
+2. String date передается в формате FEBRUARY 1 2013
+Не забудь учесть первый день года.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        System.out.println(isDateOdd("MAY 1 2013"));
+    }
+
+    public static boolean isDateOdd(String date) {
+        Date date1 = new Date(date);
+        SimpleDateFormat f = new SimpleDateFormat("D");
+
+        int a = Integer.parseInt(f.format(date1));
+
+        if(a % 2 == 0){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
