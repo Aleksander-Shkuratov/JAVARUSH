952747b405270e9e3abf7c6b54b15db9db606d4d
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-03-22 07:58:52 +0300

    log

---
+++ JavaSyntax/level9/task0906.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task09.task0906;
+
+/* 
+Логирование стек-трейса
+
+Реализовать метод log.
+Он должен выводить на экран имя класса и имя метода (в котором вызывается метод log),
+а также переданное сообщение.
+Имя класса, имя метода и сообщение разделить двоеточием с пробелом.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        log("In main method");
+    }
+
+    public static void log(String text) {
+        System.out.print(Thread.currentThread().getStackTrace()[2].getClassName()+ ": ");
+        System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName()+ ": " + text);
+        //напишите тут ваш код
+    }
+}
