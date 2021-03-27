94ff19c4c2ce602deee6b9fb3bc9886ec417ab1f
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-03-27 22:38:41 +0300

    Words - in ascending order, numbers- in descending

---
+++ JavaSyntax/level9/task0930.java
@@ -0,0 +1,95 @@
+package com.javarush.task.task09.task0930;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Задача по алгоритмам Ӏ Java Syntax: 9 уровень, 11 лекция
+
+Задача: Пользователь вводит с клавиатуры список слов (и чисел).
+Слова вывести в возрастающем порядке, числа - в убывающем.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String s = reader.readLine();
+            if (s.isEmpty()) {
+                break;
+            }
+            list.add(s);
+        }
+
+        String[] array = list.toArray(new String[0]);
+        sort(array);
+
+        for (String x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(String[] array) {
+        for(int i = 0; i < array.length-1; i++){
+            for(int j = i; j < array.length-1; j++){
+                if(!isNumber(array[i])){
+                    if(!isNumber(array[j+1])){
+                        if(isGreaterThan(array[i], array[j+1])){
+                            String tmp = array[j+1];
+                            array[j+1] = array[i];
+                            array[i] = tmp;
+                        }
+                    }
+                }
+                else{
+                    if(isNumber(array[j+1])){
+                        if(Integer.parseInt(array[j+1]) > Integer.parseInt(array[i])){
+                            String tmp = array[j+1];
+                            array[j+1] = array[i];
+                            array[i] = tmp;
+                        }
+                    }
+                }
+            }
+        }
+
+        // напишите тут ваш код
+    }
+
+    // Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+
+
+    // Переданная строка - это число?
+    public static boolean isNumber(String text) {
+        if (text.length() == 0) {
+            return false;
+        }
+
+        char[] chars = text.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char character = chars[i];
+
+            // есть '-' внутри строки
+            if (i != 0 && character == '-') {
+                return false;
+            }
+
+            // не цифра и не начинается с '-'
+            if (!Character.isDigit(character) && character != '-') {
+                return false;
+            }
+
+            // одиночный '-'
+            if (chars.length == 1 && character == '-') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
