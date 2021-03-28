78f5960d97e8595db12bf70215ccf13604b753b6
Aleksander Shkuratov <76268191+AleksanderShkuratov@users.noreply.github.com>
2021-03-28 07:42:14 +0300

    converting types 4

---
+++ JavaSyntax/level10/task1005.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task10.task1005;
+
+/* 
+Задача №5 на преобразование целых типов
+
+Правильно расставь операторы приведения типа, чтобы получился ответ: c = 256.
+int a = (byte)44;
+int b = (byte)300;
+short c = (byte)(b - a);
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = (byte) 44;
+        // System.out.println(a);
+        int b = (int) 300;
+        // System.out.println(b);
+        short c = (short) (b - a);
+        System.out.println(c);
+    }
+}
