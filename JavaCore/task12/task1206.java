b3262a5e3b4ad81003a810423341764b71dccd2f
Aleksander Shkuratov <76268191+AleksanderShkuratov@users.noreply.github.com>
2021-04-16 06:41:49 +0300

    Create task1206.java

---
+++ JavaCore/task12/task1206.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+
+Напиши два метода: print(int) и print(String).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    
+    public static void print(int a){
+        
+    }
+    
+    public static void print(String s){
+        
+    }
+
+    //Напишите тут ваши методы
+}
