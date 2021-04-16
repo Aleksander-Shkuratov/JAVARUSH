2c10e89b3c28181d16ba87300f76439ca838d5c8
Aleksander Shkuratov <76268191+AleksanderShkuratov@users.noreply.github.com>
2021-04-16 06:43:32 +0300

    Create task1208.java

---
+++ JavaCore/task12/task1208.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    
+    public static void print(int a){
+        
+    }
+    
+    public static void print(Integer b){
+        
+    }
+    
+    public static void print(String s){
+        
+    }
+    
+    public static void print(boolean c){
+        
+    }
+    
+    public static void print(int[] array){
+        
+    }
+
+    //Напишите тут ваши методы
+}
