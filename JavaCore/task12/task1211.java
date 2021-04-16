311b6b222655b485f4c73a039391957e8c99dc4d
Aleksander Shkuratov <76268191+AleksanderShkuratov@users.noreply.github.com>
2021-04-16 06:45:50 +0300

    Create task1211.java

---
+++ JavaCore/task12/task1211.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1211;
+
+/* 
+Абстрактный класс Pet
+
+Сделай класс Pet абстрактным.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    abstract static class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+    }
+
+}
