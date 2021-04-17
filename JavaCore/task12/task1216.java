50fe4d8459d3a84d2eccaacd9ce39fc3a15e4dda
Aleksander Shkuratov <76268191+AleksanderShkuratov@users.noreply.github.com>
2021-04-17 13:25:27 +0300

    Create task1216.java

---
+++ JavaCore/task12/task1216.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1216;
+
+/* 
+Летать охота
+
+Напиши свой public интерфейс CanFly(летать).
+Добавь в него два метода.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    
+    public static interface CanFly{
+        void takeoffFly();
+        void landingFly();
+    }
+    //add an interface here - добавь интерфейс тут
+
+}
