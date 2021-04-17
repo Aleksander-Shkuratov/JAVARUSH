36a578a357264c2f973c92007b6bb8383badf248
Aleksander Shkuratov <76268191+AleksanderShkuratov@users.noreply.github.com>
2021-04-17 13:26:16 +0300

    Create task1217.java

---
+++ JavaCore/task12/task1217.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+
+Напиши public интерфейсы CanFly(летать),
+CanRun(бежать/ездить), CanSwim(плавать).
+Добавить в каждый интерфейс по одному методу.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static interface CanFly{
+        void fly();
+    }
+    
+    static interface CanRun{
+        void run();
+    }
+    
+    static interface CanSwim{
+        void swim();
+    }
+//add interfaces here - добавь интерфейсы тут
+
+}
