11a6915455502ac648841b9900a19f18a7ff0ea2
Aleksander Shkuratov <76268191+AleksanderShkuratov@users.noreply.github.com>
2021-04-19 10:20:19 +0300

    Create task1218.java

---
+++ JavaCore/task12/task1218.java
@@ -0,0 +1,64 @@
+package com.javarush.task.task12.task1218;
+
+/* 
+Есть, летать и двигаться
+
+Есть public интерфейсы CanFly (летать),
+CanMove (передвигаться), CanEat (есть).
+Добавь эти интерфейсы классам Dog (собака),
+Car (автомобиль), Duck (утка), Airplane (самолет).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void fly();
+    }
+
+    public interface CanMove {
+        public void move();
+    }
+
+    public interface CanEat {
+        public void eat();
+    }
+
+    public class Dog implements CanMove, CanEat {
+        public void move(){
+            
+        }
+        public void eat(){
+            
+        }
+    }
+
+    public class Duck implements CanMove, CanEat, CanFly {
+        public void move(){
+            
+        }
+        public void eat(){
+            
+        }
+        public void fly(){
+            
+        }
+    }
+
+    public class Car implements CanMove {
+        public void move(){
+            
+        }
+    }
+
+    public class Airplane implements CanMove, CanFly {
+        public void move(){
+            
+        }
+        public void fly(){
+            
+        }
+    }
+}
